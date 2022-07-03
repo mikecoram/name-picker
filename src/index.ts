405413bc0bad851d93781754dd347d0c5702483f
mikecoram <mikecoram96@gmail.com>
2022-07-03 15:24:41 +0100

    update body text

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 const url = "https://uc67vqv73hjphy76xr2g3gyzs40nhcrw.lambda-url.eu-west-2.on.aws"
+const exampleUrl = `${url}?names=bill,jill`
 
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   const { queryStringParameters } = event
@@ -11,7 +12,10 @@ export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyRe
       <!DOCTYPE html>
       <html>
       <body>
-        bad request
+        Bad Request.
+        <br />
+        <br />
+        Try something like: <a href="${exampleUrl}">${exampleUrl}</a>
       </body>
       </html>`,
       headers: { 'content-type': 'text/html; charset=utf-8' },
@@ -50,7 +54,7 @@ export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyRe
       ${drawText}
       <br />
       <br />
-      Refresh to spin again.
+      Refresh to pick again.
     </body>
     </html>`,
     headers: { 'content-type': 'text/html; charset=utf-8' },
